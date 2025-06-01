0faa9e4b3573198bb5c9655c6a9ce058a27b25df
NY-ANDRY <nyandrypaulferdinah@gmail.com>
2025-06-01 20:26:34 +0300

    home

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,8 +1,9 @@
 <div>
+        <div class="pl-16 flex text-xl gap-8">
 
-        <h1>{{ $count }}</h1>
-        
-        <button wire:click="increment">+</button>
-        
-        <button wire:click="decrement">-</button>
+                <button wire:click="decrement">-</button>
+                <div class="w-12 flex items-center justify-center">{{ $count }}</div>
+                <button wire:click="increment">+</button>
+
+        </div>
 </div>
